bad09bfe143c1f3b391ecd802b53d094ee7e2721
Samir Medjdoub <contact@codeconcept.fr>
2017-01-27 22:32:07 +0100

    Reading a video in a player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
+import { ModalModule } from 'angular2-modal';
+import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
+
+import { AppComponent } from './app.component';
 import { FavoriteListComponent } from './favorite-list/favorite-list.component';
 import { FavoriteService } from './services/favorite.service';
 import { HomeComponent } from './home/home.component';
 import { YoutubeSearchComponent } from './youtube-search/youtube-search.component';
 import { YoutubeService } from './youtube-search/youtube.service';
+import { YoutubePlayerModule } from 'ng2-youtube-player';
 
 const ROUTES = [
   {
@@ -33,7 +37,10 @@ const ROUTES = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    ModalModule.forRoot(),
+    BootstrapModalModule,
+    RouterModule.forRoot(ROUTES),
+    YoutubePlayerModule
   ],
   providers: [ FavoriteService, YoutubeService ],
   bootstrap: [AppComponent]
--- src/app/youtube-search/youtube-search.component.ts
+++ src/app/youtube-search/youtube-search.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
+import { Overlay } from 'angular2-modal';
+import { Modal } from 'angular2-modal/plugins/bootstrap';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/debounceTime';
@@ -15,18 +19,25 @@ export class YoutubeSearchComponent implements OnInit {
 
   @ViewChild('searchterm') searchterm:ElementRef;
 
-  // eventObservable: Observable<any>;
-  
-  // results: Observable<any>;
   results: any[];
   resultsFirstArray: any[];
   resultsSecondArray: any[];
 
+  // player
+  id = 'mOD2sGp4V8o';
+  private player;
+  private ytEvent;
+  dangerousVideoUrl: string;
+  videoUrl: any; //SafeResourceUrl;
 
-  constructor(public yts:YoutubeService) {      
+
+  constructor(public yts:YoutubeService, 
+                overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal, private sanitizer: DomSanitizer) {    
+    overlay.defaultViewContainer = vcRef;  
   }
 
   ngOnInit() {
+    this.updateVideoUrl(this.id);
   }
 
   ngAfterViewInit() {
@@ -54,5 +65,35 @@ export class YoutubeSearchComponent implements OnInit {
       }
     });
   }
+
+  onClick(videoId: string) {
+    this.updateVideoUrl(videoId);
+  }
+
+  updateVideoUrl(id: string) {
+    // Appending an ID to a YouTube URL is safe.
+    // Always make sure to construct SafeValue objects as
+    // close as possible to the input data so
+    // that it's easier to check if the value is safe.
+    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + id;
+    this.videoUrl =
+        this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
+  }
+
+
+  openPopup() {
+        this.modal.alert()
+        .size('lg')
+        .showClose(true)
+        .title('un titre')
+        .body(`
+            <b>test !!!!!</b>
+            <div>
+                peut contenir du HTML
+            </div>
+            `)
+        .open();
+  }
+
 
 }
